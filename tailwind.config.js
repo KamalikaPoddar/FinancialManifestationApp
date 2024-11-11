730c46776c7e1827cbe9995a0355ceca56425024
KamalikaPoddar <154564342+KamalikaPoddar@users.noreply.github.com>
2024-11-11 12:32:58 +0530

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        // CRED-inspired color palette
+        brand: {
+          50: '#F0F4F8',
+          100: '#D9E2EC',
+          200: '#BCCCDC',
+          300: '#9FB3C8',
+          400: '#7B97A9',
+          500: '#627D98',
+          600: '#486581',
+          700: '#334E68',
+          800: '#243B53',
+          900: '#102A43',
+        }
+      },
+      borderRadius: {
+        'xl': '1rem',
+        '2xl': '1.5rem'
+      }
+    }
+  }
+}
