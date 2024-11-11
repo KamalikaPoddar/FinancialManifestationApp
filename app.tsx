e1fb20d37a77459fb2a50bdba743e060d8a23325
KamalikaPoddar <154564342+KamalikaPoddar@users.noreply.github.com>
2024-11-11 10:43:55 +0530

    Create app.tsx

---
+++ app.tsx
@@ -0,0 +1,19 @@
+import type { AppProps } from 'next/app'
+import { SessionProvider } from "next-auth/react"
+import Layout from '../components/Layout'
+import '../styles/globals.css'
+
+function MyApp({ 
+  Component, 
+  pageProps: { session, ...pageProps } 
+}: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </SessionProvider>
+  )
+}
+
+export default MyApp
